3b4d60c9fdeecd74c1958f85e57ba07a1f1b11e4
shuangjian.zhou <zhoushuangjian@blhcn.com>
2020-10-30 14:16:14 +0800

    路径开放

--- lib/idkit_button.dart
+++ lib/idkit_button.dart
@@ -1,7 +1,3 @@
 library idkit_button;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:idkit_button/base_button.dart';
