c4b950f82ee46c6bc99e29978cb61de2c83e2f7b
shuangjian.zhou <1542100658@qq.com>
2021-12-09 15:11:02 +0800

    按钮仓库初始化化

---
+++ lib/src/button_main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IDKitButton extends StatefulWidget {
+  IDKitButton({Key? key}) : super(key: key);
+
+  @override
+  _IDKitButtonState createState() => _IDKitButtonState();
+}
+
+class _IDKitButtonState extends State<IDKitButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
